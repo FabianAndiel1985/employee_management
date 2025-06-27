d19947a1848c1a5c5c420a4317ee5ab850f8a541
Fabian Andiel <fabian.andiel@gmail.com>
2025-06-27 09:19:38 +0200

    adds further code to Login Controller, adds todo

---
+++ src/main/java/org/fabianandiel/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package org.fabianandiel.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * This is a helper class to run the bean validation of Hibernate on and
+ * reduce boilerplate code
+ */
+@AllArgsConstructor
+@Getter
+public class LoginRequest {
+
+    @NotBlank(message="The username can not be empty")
+    @Size(min = 2, max = 50, message = "Your username`s length is incorrect.")
+    private String username;
+
+    @NotBlank(message="The password can not be empty")
+    @Size(min = 8, max = 16,message="Your password`s length is incorrect.")
+    private String password;
+}
