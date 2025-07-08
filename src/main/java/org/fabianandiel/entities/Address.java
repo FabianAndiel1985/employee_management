8c734e2a7d9d4cda098467d21b7184ccbfc4c477
Fabian Andiel <fabian.andiel@gmail.com>
2025-07-08 17:25:13 +0200

    annotates the address and updates the docu

--- src/main/java/org/fabianandiel/entities/Address.java
+++ src/main/java/org/fabianandiel/entities/Address.java
@@ -1,18 +1,29 @@
 package org.fabianandiel.entities;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
-//TODO add rest
+@Entity
+@Table(name = "address")
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="employee_id",nullable = false,updatable = false)
     private UUID id;
+    @Column(name="street",nullable = false,length = 100)
     private String street;
+    @Column(name="housenumber",nullable = false,length = 100)
     private String housenumber;
+    @Column(name="doornumber",nullable = false,length = 100)
     private String doornumber;
+    @Column(name="zip",nullable = false,length = 100)
     private String zip;
+    @Column(name="city",nullable = false,length = 100)
     private String city;
+    @Column(name="country",nullable = false, length = 100)
     private String country;
 
 }
