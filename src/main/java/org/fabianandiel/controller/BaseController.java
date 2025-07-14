0401e748b979c1245c8c95ff4ffa88c8ae6c9b18
Fabian Andiel <fabian.andiel@gmail.com>
2025-07-14 17:52:10 +0200

    updates Base Controller and Interface

--- src/main/java/org/fabianandiel/controller/BaseController.java
+++ src/main/java/org/fabianandiel/controller/BaseController.java
@@ -3,14 +3,36 @@ package org.fabianandiel.controller;
 import org.fabianandiel.interfaces.ControllerInterface;
 import org.fabianandiel.interfaces.DAOInterface;
 
-import java.util.UUID;
-
-public class BaseController<T,ID> implements ControllerInterface {
+public abstract class BaseController<T,ID> implements ControllerInterface<T> {
 
     private DAOInterface<T, ID> dao;
 
     public BaseController(DAOInterface<T, ID> dao) {
         this.dao = dao;
     }
+
+
+    @Override
+    public T create(T entity) {
+        try {
+            return dao.save(entity);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public T update(T entity) {
+
+        try {
+            return dao.update(entity);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+
+    }
+
+
 
 }
--- src/main/java/org/fabianandiel/interfaces/ControllerInterface.java
+++ src/main/java/org/fabianandiel/interfaces/ControllerInterface.java
@@ -1,6 +1,24 @@
 package org.fabianandiel.interfaces;
 
-public interface ControllerInterface {
+public interface ControllerInterface<T> {
+
+    /**
+     * Speichert ein neues Entity-Objekt in der Datenbank.
+     *
+     * @param entity Das zu speichernde Entity-Objekt
+     * @return Das gespeicherte Entity-Objekt mit generierter ID
+     */
+    T create(T entity);
+
+
+
+    /**
+     * Aktualisiert ein bestehendes Entity-Objekt in der Datenbank.
+     *
+     * @param entity Das zu aktualisierende Entity-Objekt
+     * @return Das aktualisierte Entity-Objekt
+     */
+    T update (T entity);
 
 
 }
