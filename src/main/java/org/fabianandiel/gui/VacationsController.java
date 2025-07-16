205c5c0c7d68d84d88264d405b13596cfea57b74
Fabian Andiel <fabian.andiel@gmail.com>
2025-07-16 11:33:12 +0200

    builds and maps vacation controller

--- src/main/java/org/fabianandiel/gui/VacationsController.java
+++ src/main/java/org/fabianandiel/gui/VacationsController.java
@@ -3,13 +3,19 @@ package org.fabianandiel.gui;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import org.fabianandiel.constants.Constants;
+import org.fabianandiel.constants.RequestStatus;
+import org.fabianandiel.context.UserContext;
+import org.fabianandiel.entities.Request;
 import org.fabianandiel.services.GUIService;
 import org.fabianandiel.services.SceneManager;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
 public class VacationsController implements Initializable {
@@ -20,9 +26,22 @@ public class VacationsController implements Initializable {
     @FXML
     private Text vacationsErrorText;
 
+    @FXML
+    private Button vacationsSubmit;
+
+    @FXML
+    private DatePicker vacationsStartDate;
+
+    @FXML
+    private DatePicker vacationsEndDate;
+
+    @FXML
+    private TextField vacationsNotes;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 
     public void goBackToMainView() {
@@ -38,6 +57,23 @@ public class VacationsController implements Initializable {
     }
 
 
+    public void submit(){
+        Request request = new Request();
+        setPreKnownData(request);
+
+
+
+        System.out.println(request);
+
+        //ToDo Am Ende Person im User Context updaten
+    }
+
+
+    private void setPreKnownData(Request request) {
+        request.setStatus(RequestStatus.PENDING);
+        request.setCreationDate(LocalDateTime.now());
+        request.setCreator(UserContext.getInstance().getPerson());
+    }
 
 
 }
