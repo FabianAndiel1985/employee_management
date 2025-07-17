d68b46faf236f8480802730ecf68335930d916ac
Fabian Andiel <fabian.andiel@gmail.com>
2025-07-17 10:04:07 +0200

    starts setting up pending requests view for managers

--- src/main/java/org/fabianandiel/controller/RequestController.java
+++ src/main/java/org/fabianandiel/controller/RequestController.java
@@ -1,12 +1,19 @@
 package org.fabianandiel.controller;
 
 import org.fabianandiel.dao.RequestDAO;
-import org.fabianandiel.dao.TimeStampDAO;
+
+import org.fabianandiel.entities.Person;
+import org.fabianandiel.entities.Request;
 import org.fabianandiel.interfaces.DAOInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class RequestController<T,ID> extends BaseController<T,ID>{
 
     private RequestDAO<T, ID> requestDAO;
+
 
     public RequestController(DAOInterface<T, ID> dao) {
         super(dao);
@@ -15,5 +22,16 @@ public class RequestController<T,ID> extends BaseController<T,ID>{
         } else {
             throw new IllegalArgumentException("Invalid DAO.");
         }
+    }
+
+    public List<Request> getPendingRequestsOfSubordinates(Set<Person> subordinates) {
+
+        List<Request> pendingRequest = new ArrayList<>();
+
+        subordinates.stream().forEach((person)->{
+
+        });
+
+        return null;
     }
 }
--- src/main/java/org/fabianandiel/gui/RequestsController.java
+++ src/main/java/org/fabianandiel/gui/RequestsController.java
@@ -1,16 +1,27 @@
 package org.fabianandiel.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 import org.fabianandiel.constants.Constants;
+import org.fabianandiel.context.UserContext;
+import org.fabianandiel.controller.RequestController;
+import org.fabianandiel.dao.RequestDAO;
+import org.fabianandiel.entities.Person;
+import org.fabianandiel.entities.Request;
 import org.fabianandiel.services.GUIService;
 import org.fabianandiel.services.SceneManager;
-
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class RequestsController implements Initializable {
 
@@ -20,10 +31,37 @@ public class RequestsController implements Initializable {
     @FXML
     private Text requestsErrorText;
 
+    @FXML
+    private TableView<Request> pendingRequestsTableView;
+
+    @FXML
+    private TableColumn<Request, LocalDate> pendingRequestsStartDate;
+
+    @FXML
+    private TableColumn<Request, LocalDate> pendingRequestsEndDate;
+
+    @FXML
+    private TableColumn<Request, String> pendingRequestsNotes;
+
+    private final ObservableList<Request> pendingRequestsList = FXCollections.observableArrayList();
+
+    private RequestController requestController = new RequestController<>(new RequestDAO());
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //TODO get all the pending requests of all subordintes
+
+        Set<Person> subordinates = UserContext.getInstance().getPerson().getSubordinates();
+
+        if(subordinates.isEmpty()){
+            //TODO show error text that no subordinates are found
+            return;
+        }
+
+        //TODO buttons adden
+
+        List<Request> pendingRequests = requestController.getPendingRequestsOfSubordinates(subordinates);
 
     }
 
